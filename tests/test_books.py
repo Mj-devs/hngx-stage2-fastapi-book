0f0af5e7ff4df56f3deeb75dd81fdf4899934c49
Mj-Devs <developersholotan@gmail.com>
2025-02-13 06:25:21 +0100

    Added changes for CI testing

--- tests/test_books.py
+++ tests/test_books.py
@@ -2,13 +2,13 @@ from tests import client
 
 
 def test_get_all_books():
-    response = client.get("/books/")
+    response = client.get("/")
     assert response.status_code == 200
     assert len(response.json()) == 3
 
 
 def test_get_single_book():
-    response = client.get("/books/1")
+    response = client.get("/1")
     assert response.status_code == 200
     data = response.json()
     assert data["title"] == "The Hobbit"
@@ -23,7 +23,7 @@ def test_create_book():
         "publication_year": 1997,
         "genre": "Fantasy",
     }
-    response = client.post("/books/", json=new_book)
+    response = client.post("/", json=new_book)
     assert response.status_code == 201
     data = response.json()
     assert data["id"] == 4
@@ -38,14 +38,14 @@ def test_update_book():
         "publication_year": 1937,
         "genre": "Fantasy",
     }
-    response = client.put("/books/1", json=updated_book)
+    response = client.put("/1", json=updated_book)
     assert response.status_code == 200
     data = response.json()
     assert data["title"] == "The Hobbit: An Unexpected Journey"
 
 
 def test_delete_book():
-    response = client.delete("/books/3")
+    response = client.delete("/3")
     assert response.status_code == 204
 
     response = client.get("/books/3")
